976284830197e8f4965b1a1189f80ae23adb8245
Nethminavanjana <nethushanu833@gmail.com>
2026-03-15 12:00:03 +0530

    first

---
+++ food-delivery-app/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Logo from "../assets/Logo.svg";
+import {BsCart2} from "react-icons/bs";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import { Box,Drawer,ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+
+const Navbar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navbar
